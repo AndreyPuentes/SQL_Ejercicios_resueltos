2095c910987f7b3eca925c9e6c0349b11f8a3e15
Camilo Puentes <acpuentesm@unal.edu.co>
2022-03-10 15:21:11 -0500

    Se han cargado los ejercicios de BD SQL realizados

---
+++ Exercices_Leetcode/Nth Highest Salary.sql
@@ -0,0 +1,66 @@
+/*Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the nth highest salary from the Employee table. If there is no nth highest salary, the query should report null.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+*/
+
+CREATE FUNCTION getNthHighestSalary(@N INT) 
+RETURNS INT 
+AS
+BEGIN
+    DECLARE @RESULT INT
+    /* Write your T-SQL query statement below. */
+    SET @RESULT = (SELECT DISTINCT salary AS getNthHighestSalary
+                   FROM Employee
+                   ORDER BY salary DESC
+                   OFFSET (@N-1) ROWS 
+                   FETCH NEXT 1 ROWS ONLY)
+    RETURN @RESULT;
+END
